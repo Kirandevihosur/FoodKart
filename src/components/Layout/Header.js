878186694537f3598a64691168dcbc7302e85454
kiran-devihosur <kiran-devihosur@teletracking.com>
2022-02-15 14:51:55 +0530

    Adding Context

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import mealsImage from '../../assets/meals.jpg'
 import styles from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
     return(
         <React.Fragment>
             <header className={styles.header}>
                 <h1>FoodKart.com</h1>
-                <button>Cart</button>
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={styles['main-image']}>
                 <img src={mealsImage} alt='A table full of delicious food!'/>
