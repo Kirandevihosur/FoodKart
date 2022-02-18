d23b7309c218399daa81918ba9839b84837e66c8
kiran-devihosur <kiran-devihosur@teletracking.com>
2022-02-18 19:29:21 +0530

    adding meals data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,64 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import classes from './AvailableMeals.module.css';
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+//import axios from "axios";
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meal)=> <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} id={meal.id}/>);
+
+    const [meals, setMeals] = useState([]);
+
+  // Getting Meals data using Axios  
+  //   useEffect(()=>{
+  //    axios.get('https://foodkart-8d5ed-default-rtdb.firebaseio.com/meals.json').then(
+  //     (response)=>{
+  //       const data = response.data;
+  //       const loadedMeals = [];
+
+  //       for(const key in data){
+  //         loadedMeals.push({
+  //           id: key,
+  //           name: data[key].name,
+  //           description: data[key].description,
+  //           price: data[key].price
+  //         });
+  //     }
+  //     setMeals(loadedMeals);
+  //   });
+  // },[]);
+
+    
+     
+   
+
+    useEffect(()=>{
+      const fetchMealsHandler = async()=>{
+        const response = await fetch('https://foodkart-8d5ed-default-rtdb.firebaseio.com/meals.json');
+        const responseData = await response.json();
+
+        const loadedMeals = [];
+
+        for(const key in responseData){
+          loadedMeals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price
+          });
+        }
+        setMeals(loadedMeals);
+    };
+     fetchMealsHandler();   
+  },[]);
+
+    
+
+    let mealsList = meals.map((meal)=>{
+      return <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} id={meal.id} />
+    })
+    
+    //DUMMY_MEALS.map((meal)=> <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} id={meal.id}/>);
+
     return(
         <section className={classes.meals}>
         <Card>
